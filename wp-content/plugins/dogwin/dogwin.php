48676a392765661648a756ece195d02da4bcf063
darren <darren.miao@e-graphics.com>
2013-03-12 18:00:27 +0800

    clear

--- wp-content/plugins/dogwin/dogwin.php
+++ wp-content/plugins/dogwin/dogwin.php
@@ -6,3 +6,17 @@
  * Author: dogwin
  * Author URI: http://www.dogwin.net
  */
+global $wp_version;
+if(!version_compare($wp_version,'3.5',">=")):
+	die('the big');
+endif;
+
+function my_plugin_activate(){
+	error_log('plugin activated');
+}
+register_activation_hook(__FILE__,"my_plugin_activate");
+
+function my_plugin_deactivate(){
+	error_log('plugin deactivated');
+}
+register_deactivation_hook(__FILE__,"my_plugin_deactivate");
